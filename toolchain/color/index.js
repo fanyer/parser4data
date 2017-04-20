fcac2d313ac0399dec0255a9af277898e506c1d1
fanyer <iofanyer@gmail.com>
2017-04-20 16:44:04 +0800

    toolchain

---
+++ toolchain/color/index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+function find(url,text='keyword') {
+  
+}
+
+function match(argument) {
+  // body...
+}
+
+function updateOne(argument) {
+  // body...
+};
+function updateAll(argument) {
+  // body...
+}
+
