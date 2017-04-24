b2ed80cca1b08a19b472e1e84ae217bc69f050e6
fanyer <iofanyer@gmail.com>
2017-04-24 23:51:17 +0800

    mock-data

---
+++ mock-data/link.raw.js
@@ -0,0 +1,54 @@
+/**
+ * left length 16
+ */
+export default {
+    "left": [{
+        "name": "维生素A",
+        "color": "#00ab84"
+    }, {
+        "name": "维生素B1",
+        "color": "#e4be6f"
+    }, {
+        "name": "维生素B2",
+        "color": "#e4be6f"
+    }, {
+        "name": "维生素B3",
+        "color": "#e4be6f"
+    }, {
+        "name": "维生素B5",
+        "color": "#e4be6f"
+    }, {
+        "name": "维生素B6",
+        "color": "#e4be6f"
+    }, {
+        "name": "维生素B7",
+        "color": "#e4be6f"
+    }, {
+        "name": "维生素B9",
+        "color": "#e4be6f"
+    }, {
+        "name": "维生素B12",
+        "color": "#cb8d88"
+    }, {
+        "name": "维生素C",
+        "color": "#e4be6f"
+    }, {
+        "name": "胡萝卜素",
+        "color": "#e4be6f"
+    }, {
+        "name": "维生素E",
+        "color": "#e4be6f"
+    }, {
+        "name": "牛磺酸",
+        "color": "#cb8d88"
+    }, {
+        "name": "辅酶Q",
+        "color": "#e4be6f"
+    }, {
+        "name": "异黄酮",
+        "color": "#e4be6f"
+    }, {
+        "name": "维生素K",
+        "color": "#e4be6f"
+    }]
+}
