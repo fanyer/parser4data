a1aca5a6b1473b62b69cd840502cdf88fc3568d0
fanyer <iofanyer@gmail.com>
2017-05-16 14:59:21 +0800

    rb interface for link parser

--- rb/link.parser.rb
+++ rb/link.parser.rb
@@ -1,19 +1,55 @@
 #!/usr/bin/env ruby
 # coding:utf-8
-require "rubygem"
 require "json"
 require "pp"
 
-rawJson=File.open('../mocks-data/link-graph.raw.json').read
-rawConfigObj=JSON.parse(rawJson)
-
-baseJson=File.open('../default-config/link-graph.config.json').read
-baseConfigObj=JSON.parse(baseJson)
+# rawJson=File.open("../mock-data/data.csv").read
+rawLink=File.open("../mock-data/link.raw.json").read
+input=JSON.parse(rawLink)["left"]
 
 
-# While this creates a new default object each time
-h = Hash.new { |hash, key| hash[key] = "linkGraph: #{key}" }
-h["nodes"]
-h["links"]
+baseLink=File.open("../default-config/link.config.json").read
+baseJson=JSON.parse(baseLink)
+nodes=baseJson["nodes"]
 
-output=Hash.new("default linkGraph")
+input.each.with_index { |e, i| nodes[i]["color"]=e["color"]}
+
+
+colorsA=[]
+linkValues={
+  '#00ab84'=>5,
+  '#e4be6f'=>5,
+  '#cb8d88'=>9
+}
+
+
+output=Hash.new("default")
+
+arrN=[]
+arrL=[]
+
+input.map.with_index { |elem,index|
+
+  linksFinded=baseJson["links"].select { |e|
+    e["source"]===index
+  }
+
+  linksFinded.each.with_index { |e,i|
+    if colorsA.include?(elem)
+      colorsA<< elem["color"]
+    end
+
+    e["color"]=elem["color"]
+
+    e["strokewidth"]=linkValues[elem["color"]]
+
+    for i in 0...linksFinded.length
+      arrL<<linksFinded[i]
+    end
+  }
+}
+
+output[:nodes]=arrN
+output[:links]=arrL
+
+p output.to_json
