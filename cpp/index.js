85bdd71a3c0d5b3d7a90275087897ac45cc5eede
fanyer <iofanyer@gmail.com>
2017-05-25 18:01:48 +0800

    migrating

--- cpp/index.js
+++
@@ -1,5 +0,0 @@
-const Parser = require('./build/Release/parser');
-
-export default {
-    parse: Parser.parse
-};
--- index.js
+++
@@ -1,7 +0,0 @@
-Object.values = x =>
-    Object.keys(x).reduce((y, z) =>
-        y.push(x[z]) && y, []);
-
-
-export  {parse as antibioticsParse} from './js/estimate-antibiotics.parser'
-export {parse as linkParse} from './js/link-graph.parser'
